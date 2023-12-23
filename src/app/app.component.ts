4bb9ce1be09df93702387bb70b69e985ef72e7b8
Juan Francisco Mosquera <maxiplux@gmail.com>
2023-12-23 10:11:10 -0500

    first commit
    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +11,11 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
+
   title = 'angular2024';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
